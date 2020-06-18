1fd19ef06c2b240b18ef6d8c5aea06d9ebc69d66
Will Johnston <wijohnst@gmail.com>
2020-06-18 13:43:49 -0400

    changed background color

--- utils/getColor.js
+++ utils/getColor.js
@@ -6,7 +6,7 @@ export const getColor = color =>{
     accent: '#13efc3',
     accent_black: '#333333',
     accent_white: '#FFFFFF',
-    accent_grey: '#808080'
+    accent_grey: '#5C5C5C'
   }
 
   return colors[color];
